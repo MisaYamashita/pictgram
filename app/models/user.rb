4c94f0787138540e2b5bd3c3845e0d8c90b4e214
misa <mi-426-sa@hotmail.co.jp>
2020-08-31 09:04:15 +0000

    課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  validates :name, presence: true,
+                   length: {maximum: 15}
+                   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: {with: VALID_EMAIL_REGEX}
+                      
+  VAILD_PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  validates :password, presence: true,
+                       format: {with: VAILD_PASSWORD_REGEX}
+  validates :password_confirmation, presence: true,
+                                    format: {with: VAILD_PASSWORD_REGEX}
+  
+  has_secure_password
 end
