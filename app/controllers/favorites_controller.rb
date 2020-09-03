708c6d7a222538552bd673fc5303da20f0fa4092
misa <mi-426-sa@hotmail.co.jp>
2020-09-03 12:53:11 +0000

    課題提出07

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,23 @@ class FavoritesController < ApplicationController
   end
   
   def create
-    favorite = Favorite.new
-    favorite.user_id = current_user.id
-    favorite.topic_id = params[:topic_id]
+    @favorite = Favorite.new
+    @favorite.user_id = current_user.id
+    @favorite.topic_id = params[:topic_id]
     
-    if favorite.save
+    if @favorite.save
       redirect_to topics_path, success: 'お気に入りに登録しました'
     else
       redirect_to topics_path, danger: 'お気に入り登録に失敗しました'
     end 
+  end
+  
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    @favorite.destroy
+    redirect_to topics_path, success:  "お気に入り登録を解除しました"
   end 
+      
+  
+  
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   
   get 'favorites/index'
   post 'favorites', to: 'favorites#create'
+  delete 'favorites', to: 'favorites#destroy' 
   
 end
